33956e76f0ca2ac8ca33d5023cd9f8d74953267e
elijah-darkwood <99073013+elijah-darkwood@users.noreply.github.com>
2022-03-29 10:51:53 +0300

    remove banner on listings page and update logo

--- js/packages/web/src/views/home/components/SalesList/index.tsx
+++ js/packages/web/src/views/home/components/SalesList/index.tsx
@@ -41,7 +41,7 @@ export const SalesListView = (props: { collectionMintFilter?: string }) => {
 
   return (
     <>
-      {!props.collectionMintFilter && (
+      {/* {!props.collectionMintFilter && (
         <Banner
           src="/main-banner.svg"
           headingText="The amazing world of Metaplex."
@@ -49,7 +49,7 @@ export const SalesListView = (props: { collectionMintFilter?: string }) => {
           actionComponent={<HowToBuyModal buttonClassName="secondary-btn" />}
           useBannerBg
         />
-      )}
+      )} */}
       <Layout>
         <Content style={{ display: 'flex', flexWrap: 'wrap' }}>
           <Col style={{ width: '100%', marginTop: 32 }}>
